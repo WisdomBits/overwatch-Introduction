4d990c9a7956aaac9335647d1f5802d525e99d65
karandeepsingh7070 <soundportrait2530@gmail.com>
2025-07-02 18:48:36 +0530

    jugnu effect

--- src/particlesjs-config-light.js
+++ src/particlesjs-config-light.js
@@ -25,13 +25,11 @@ export const particleConfigLight = {
         "height": 100
       }
     },
-    "opacity": {
-      "value": 1,
-      "random": true,
-      "anim": {
+      "opacity": {
+      "value": { min: 0.1, max: 1 },
+      "animation": {
         "enable": true,
-        "speed": 1,
-        "opacity_min": 0,
+        "speed": 2,
         "sync": false
       }
     },
--- src/particlesjs-config.js
+++ src/particlesjs-config.js
@@ -26,12 +26,10 @@ export const particleConfigDark = {
       }
     },
     "opacity": {
-      "value": 1,
-      "random": true,
-      "anim": {
+      "value": { min: 0.1, max: 1 },
+      "animation": {
         "enable": true,
-        "speed": 1,
-        "opacity_min": 0,
+        "speed": 2,
         "sync": false
       }
     },
