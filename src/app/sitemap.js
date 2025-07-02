ae66e558f855f75252c3e6d496c3b97478f6e5d8
karandeepsingh7070 <soundportrait2530@gmail.com>
2025-07-02 15:41:27 +0530

    sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,16 @@
+export default async function sitemap() {
+    return [
+        {
+            url : "https://overwatchts.in/",
+            lastModified : new Date()
+        },
+        {
+            url : "https://overwatchts.in/docs",
+            lastModified : new Date()
+        },
+         {
+            url : "https://overwatchts.in/blog",
+            lastModified : new Date()
+        },
+    ]
+}
